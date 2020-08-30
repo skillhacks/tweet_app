64375d9789c7b0db9ec87265dd0f41f361d7f032
skliihacks <juventus01tot@yahoo.co.jp>
2020-08-31 01:24:26 +0900

    edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,31 @@ class PostsController < ApplicationController
     def new
 
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
 
     def create
         Post.create(title: params["posts"]["title"],body:params["posts"]["body"])
+        redirect_to "/posts"
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end 
+
+    def update
+        post = Post.find(params["id"])
+        post.title = params["posts"]["title"]
+        post.body = params["posts"]["body"]
+        post.save
+        redirect_to "/posts"
+    end
+
+    def destroy
+        post = Post.find(params["id"])
+        post.destroy
         redirect_to "/posts"
     end
 end
